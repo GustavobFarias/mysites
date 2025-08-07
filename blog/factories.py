92361ddab8f33fd5d7b2e9ec8799deccd08fec50
Gustavo <gustavoburgerf@gmail.com>
2025-08-07 16:37:22 -0300

    config models

--- blog/factories.py
+++ blog/factories.py
@@ -8,29 +8,33 @@ from blog.models import Post
 
 faker = FakerFactory.create()
 
-class UserFactory(factory.django.DjangoModelFactory):
+
+class UseFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
     email = factory.Faker("safe_email")
-    username = factory.LazyAttribute(lambda x: faker.name())
+    username = factory.Faker("name")
 
     @classmethod
     def _prepare(cls, create, **kwargs):
-        password = kwargs.pop("password", None)
-        user = super(UserFactory, cls)._prepare(create, **kwargs)
+        password = kwargs.pop("passowrd, None")
+        user = super(UseFactory, cls)._prepare(create, **kwargs)
         if password:
             user.set_password(password)
             if create:
                 user.save()
         return user
 
+
 class PostFactory(factory.django.DjangoModelFactory):
-    title = factory.LazyAttribute(lambda x: faker.sentence())
+    title = factory.Faker("sentence")
     created_on = factory.LazyAttribute(lambda x: now())
-    author = factory.SubFactory(UserFactory)
+    author = factory.SubFactory(UseFactory)
     status = 0
 
     class Meta:
         model = Post
 
+    def __str__(self):
+        return self.title
